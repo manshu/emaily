e0a7403b3a6d7905a12b29a69a87f4d63b3f37f4
Himanshu Batra <himanshu@batra.io>
2019-03-31 10:56:25 -0400

    WiP

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const app = express();
 
-app.get("/", (req, res) => {
+passport.use(new GoogleStrategy());
+
+app.get("/auth/google", (req, res) => {
   res.send({ hi: "there" });
 });
 
